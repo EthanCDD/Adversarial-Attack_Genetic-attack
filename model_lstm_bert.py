38047013dcb023607124c3afb86c07836bd71713
EthanCDD <dc15426@my.bristol.ac.uk>
2020-07-24 12:14:59 +0800

    update

--- model_lstm_bert.py
+++ model_lstm_bert.py
@@ -10,13 +10,13 @@ from torch import nn
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 
 class bert_lstm(nn.Module):
-  def __init__(self, bert, output_size, bidirection, n_layer, hidden_size, freeze, kept_prob):
+  def __init__(self, bert, output_size, bidirection, n_layer, hidden_size, freeze=True, kept_prob = 1):
     super().__init__()
     self.n_layer = n_layer
     self.hidden_size = hidden_size
     self.bert = bert
     if freeze:
-      self.freeze_bert()
+        self.freeze_bert()
     embedding_size = bert.config.to_dict()['hidden_size']
     self.lstm = nn.LSTM( input_size=embedding_size,
                 hidden_size=hidden_size, 
@@ -26,29 +26,42 @@ class bert_lstm(nn.Module):
     if bidirection == True:
         bidirect = 2
     else:
-        bidirect = 1
-    self.fc = nn.Linear(bidirect*hidden_size, output_size)
+        bidirect = 1    
+    self.fc = nn.Linear(hidden_size*bidirect, output_size)
     self.dropout = nn.Dropout(1-kept_prob)
     self.softmax = nn.Softmax(1)
     self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-  def forward(self, text, l):
+  def forward(self, text, l, is_train):
+    h_0, c_0 = self.h_c_initialisation(text.shape[0])
+    h_0, c_0 = h_0.cuda(), c_0.cuda()
 
     embedding_mat = self.bert(text)[0]
     batch_size, seq_len, emb_dim = embedding_mat.shape
     
+#    h_0, c_0 = self.hidden_b(batch_size)
     bert_seqs = embedding_mat.permute(1,0,2)
-    bert_seqs = self.dropout(bert_seqs)
+    if is_train:
+        bert_seqs = self.dropout(bert_seqs)
     packed_seqs = pack_padded_sequence(bert_seqs, l)
-    output, (h,c) = self.lstm(packed_seqs)
+    output, (h,c) = self.lstm(packed_seqs, (h_0, c_0))
     output = pad_packed_sequence(output)
     
-    lstm_output = torch.mean(output[0], dim = 0)
-    lstm_output = self.dropout(lstm_output)
+    lstm_output = torch.sum(output[0], dim = 0)/l.type(torch.float).unsqueeze(1)
+    if is_train:
+        lstm_output = self.dropout(lstm_output)
     output = self.fc(lstm_output)
-    output = self.softmax(output)
+    pred_out = self.softmax(output)
 
-    return output
+    return output, pred_out
+
+  def h_c_initialisation(self, batch):
+    h = torch.zeros(self.n_layer, batch, self.hidden_size)
+    c = torch.zeros(self.n_layer, batch, self.hidden_size)
+
+    h = h.type(torch.float)
+    c = c.type(torch.float)
+    return h, c
 
 
   def freeze_bert(self):
@@ -59,5 +72,5 @@ class bert_lstm(nn.Module):
     v = np.sum(np.argmax(pred, 1) == target)
     return v/len(target)
 
-  def pred(self, seq, l):
-    return self.forward(seq, l)
+  def pred(self, seq, l, is_train):
+    return self.forward(seq, l, is_train)
